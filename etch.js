b73f257dcd3315a2cc974dcc3df3ed03fd40299c
Logan Swanson <logswan9@gmail.com>
2024-07-23 22:40:11 -0500

    added etch-a-sketch functionality

--- etch.js
+++ etch.js
@@ -2,13 +2,19 @@
 
 let containerDiv = document.getElementById("container");
 
+
 for (var i = 1; i < 17; i ++) {
     var innerContainerDiv = document.createElement("div");
     innerContainerDiv.className = "flex-container";
     containerDiv.appendChild(innerContainerDiv);
     for (var z = 1; z < 17; z ++) {
         var div = document.createElement("div");
-        div.className = "box" + z;
+        //div.className = "box";
         innerContainerDiv.appendChild(div);
+        div.addEventListener('mouseover', (event) => {
+            event.target.className = "box";
+        });
+       
     }
+    
 }
