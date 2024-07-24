87a5f9d3818c9ca4b0058c375e2263a341110301
Logan Swanson <logswan9@gmail.com>
2024-07-23 23:04:46 -0500

    added functionality for resizing grid with a button. Handles errors for numbers over 100 and below 1

--- etch.js
+++ etch.js
@@ -1,20 +1,41 @@
-
-
-let containerDiv = document.getElementById("container");
-
-
-for (var i = 1; i < 17; i ++) {
-    var innerContainerDiv = document.createElement("div");
-    innerContainerDiv.className = "flex-container";
-    containerDiv.appendChild(innerContainerDiv);
-    for (var z = 1; z < 17; z ++) {
-        var div = document.createElement("div");
-        //div.className = "box";
-        innerContainerDiv.appendChild(div);
-        div.addEventListener('mouseover', (event) => {
-            event.target.className = "box";
-        });
-       
+document.getElementById("resetBtn").addEventListener('click', () => {
+    deleteGrid();
+    var size;
+    while (isNaN(size) || size > 100 || size < 1) {
+        size = prompt("Enter a new size for the grid:")
+        if (isNaN(size) || size > 100 || size < 1) {
+            alert("Invalid size. PLease enter a positive number below 100");
+        }
     }
-    
+    setGrid(size);
+});
+
+setGrid(16);
+
+function setGrid (gridSize) {
+    let containerDiv = document.getElementById("container");
+
+    for (var i = 0; i < gridSize; i ++) {
+        var innerContainerDiv = document.createElement("div");
+        innerContainerDiv.className = "flex-container";
+        containerDiv.appendChild(innerContainerDiv);
+        for (var z = 0; z < gridSize; z ++) {
+            var div = document.createElement("div");
+            //div.className = "box";
+            innerContainerDiv.appendChild(div);
+            div.addEventListener('mouseover', (event) => {
+                event.target.className = "hovered";
+            });
+        
+        }
+        
+    }
 }
+
+function deleteGrid() {
+    document.getElementById("container").remove();
+    var newDiv = document.createElement("div");
+    newDiv.id = "container";
+    document.getElementsByTagName("body")[0].appendChild(newDiv)
+}
+
