a7a92bb282d818a27abc90bed4dc53b0e4957645
Logan Swanson <logswan9@gmail.com>
2024-07-23 22:12:42 -0500

    grid created with red squares

--- etch.js
+++ etch.js
@@ -4,7 +4,7 @@ let containerDiv = document.getElementById("container");
 
 for (var i = 1; i < 17; i ++) {
     var innerContainerDiv = document.createElement("div");
-    innerContainerDiv.className = "flex-container" + i;
+    innerContainerDiv.className = "flex-container";
     containerDiv.appendChild(innerContainerDiv);
     for (var z = 1; z < 17; z ++) {
         var div = document.createElement("div");
