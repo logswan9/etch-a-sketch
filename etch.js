6bc5125f2bc628f8dc432f5f3c78c805a54ca59f
Logan Swanson <logswan9@gmail.com>
2024-07-23 22:07:45 -0500

    added 16x16 divs through js

--- etch.js
+++ etch.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+
+
+let containerDiv = document.getElementById("container");
+
+for (var i = 1; i < 17; i ++) {
+    var innerContainerDiv = document.createElement("div");
+    innerContainerDiv.className = "flex-container" + i;
+    containerDiv.appendChild(innerContainerDiv);
+    for (var z = 1; z < 17; z ++) {
+        var div = document.createElement("div");
+        div.className = "box" + z;
+        innerContainerDiv.appendChild(div);
+    }
+}
